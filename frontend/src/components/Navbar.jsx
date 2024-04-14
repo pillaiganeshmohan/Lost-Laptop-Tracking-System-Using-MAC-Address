39de5739e2ab2fe887549c8ac480ea6e74be4131
C0SMlC <pendurkarpratik04@gmail.com>
2024-04-14 15:54:56 +0530

    Revert "feat: make website responsive for smaller screen sizes"
    This reverts commit 18dfaad6f888da0fef462d3406aa943b3a01d9d1.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,49 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../assets/Logo.png";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/Logo.png'
 
 function Navbar() {
   return (
-    <div className="w-full flex bg-black h-full z-20 justify-between">
-      <div className="w-1/6 flex items-center">
-        <img className="w-20 h-12 ml-10 flex" src={logo} alt="Logo" />
-      </div>
-      <div className="w-3/6 flex items-center justify-end gap-12 nav-links">
-        <Link className="text-white hover:underline" to="/">
-          Home
-        </Link>
-        <Link className="text-white hover:underline" to="/walkthrough">
-          Walkthrough
-        </Link>
-        <Link className="text-white hover:underline" to="/about-us">
-          About Us
-        </Link>
-        <Link className="text-white hover:underline" to="/contact-us">
-          Contact Us
-        </Link>
-      </div>
-      <div className="w-1/6 flex items-center justify-evenly nav-links">
-        <Link className="text-white hover:underline" to="/login">
-          Login
-        </Link>
-        <Link className="text-white hover:underline" to="/signup">
-          Sign Up
-        </Link>
-      </div>
+    <div className='w-full flex bg-black h-full z-20 justify-between'>
+        <div className='w-1/6 flex items-center'>
+            <img className='w-20 h-12 ml-10 flex' src={logo}/>
+        </div>
+        <div className='w-3/6 flex items-center justify-end gap-12'>
+            <Link className='text-white text-xl hover:underline' to='/'>Home</Link>
+            <Link className='text-white text-xl hover:underline'  to='/walkthrough'>Walkthrough</Link>
+            <Link className='text-white text-xl hover:underline' to='/about-us'>About Us</Link>
+            <Link className='text-white text-xl hover:underline' to='/contact-us'>Contact Us</Link>
 
-      <style jsx>{`
-        .nav-links a {
-          font-size: 1.25rem; /* 20px */
-        }
+        </div>
+        <div className='w-1/6 flex items-center justify-evenly'>
+        <Link className='text-white text-xl hover:underline' to='/login'>Login</Link>
+        <Link className='text-white text-xl hover:underline' to='/signup'>Sign Up</Link>
+        </div>
 
-        @media (max-width: 600px) {
-          .nav-links a {
-            font-size: 1rem; /* 16px */
-          }
-        }
-      `}</style>
     </div>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
